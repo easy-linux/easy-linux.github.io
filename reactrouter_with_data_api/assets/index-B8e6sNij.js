7acc3d10c82366f17cc027f7957d00eca4ada7e2
Easy IT <its.easy.linux@gmail.com>
2024-04-26 12:05:56 +0300

    added working code for react router data api

---
+++ reactrouter_with_data_api/assets/index-B8e6sNij.js
@@ -0,0 +1,1 @@
+import{j as s,L as t,O as o}from"../main.js";const e=()=>s.jsxs("div",{className:"app-container",children:[s.jsxs("div",{className:"toolbar",children:[s.jsx(t,{to:"/",className:"toolbar-btn",children:"Home"}),s.jsx(t,{to:"/posts",className:"toolbar-btn",children:"Posts"}),s.jsx(t,{to:"/todos",className:"toolbar-btn",children:"Tickets"})]}),s.jsx(o,{})]});export{e as default};
